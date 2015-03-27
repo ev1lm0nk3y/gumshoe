e61cdfeba50abae32b8f306d73946871db2f8835
Ryan Shatford <evilmonkey@gmail.com>
2015-03-27 01:56:30 -0700

    new func getInt means I don't have to include strconv in every file to convert string to int

--- gumshoe/misc.go
+++ gumshoe/misc.go
@@ -1,9 +1,16 @@
 package gumshoe
 
-import "log"
+import (
+  "log"
+)
 
 func checkErr(err error, msg string) {
 	if err != nil {
 		log.Fatalln(msg, err)
 	}
+}
+
+func getInt(s string) int {
+	r, _ := strconv.Atoi(s)
+	return r
 }
